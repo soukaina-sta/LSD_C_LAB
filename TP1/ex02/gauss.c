894fa03f4a1925094c4df571e1ee0503df567817
soukaina-sta <75736345+soukaina-sta@users.noreply.github.com>
2021-02-02 21:18:38 +0100

    Update gauss.c

--- TP1/ex02/gauss.c
+++ TP1/ex02/gauss.c
@@ -5,22 +5,47 @@
 float *remontee(float A[N][N], float B[N], int n)
 {
 
-  /******Implement the solution here******/
-  /***************************************/
+   int i,j;
+  float *x;
+  float s;
+  x = malloc(sizeof(float) * n);
+   x[n - 1] = B[n - 1] / A[n -1][n - 1];
   
-  /***************************************/
+  for (i = n - 2; i >= 0; i--)
+  {
+     float s = 0;
+     for (j = i + 1; j < n; j++)
+     {
+	     s += A[i][j] * x[j];
+     }
+     x[i] = (B[i] - s) / A[i][i];
+  }
+
   return x;
 }
 
 float *gauss(float A[N][N] ,float B[N], int n)
 {
-  /******Implement the Gaussian elimination******/
-  /**********************************************/
-  
+ int i,j,k;
+  float *x;
 
-  /**********************************************/
-  /*Resolve the system using the back substitution method*/
-  /*You can use your solution of the exercice 1-b*/
+  x = malloc(sizeof(float) * n);
+
+  
+  for (k = 0; k < n - 1; k++)
+  {
+	 for (i = k + 1; i < n; i++)
+	 {
+		 float F = A[i][k] / A[k][k];
+		 
+		 for (j = k + 1; j < n; j++)
+		 {
+			 A[i][j] = A[i][j] - F * A[k][j];
+		 }
+		 B[i] = B[i] - F * B[k];
+	 }
+  } 
+  x = remontee(A, B, n);
   x = remontee(A, B, n);
   return(x);
 }
