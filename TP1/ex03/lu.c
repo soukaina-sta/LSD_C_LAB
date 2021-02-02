1901941c3fa1700daae6474a8453831563f6dd39
soukaina-sta <75736345+soukaina-sta@users.noreply.github.com>
2021-02-02 21:16:07 +0100

    Update lu.c

--- TP1/ex03/lu.c
+++ TP1/ex03/lu.c
@@ -4,12 +4,73 @@
 
 float *lu(float A[N][N], float B[N], int n)
 {
+   int i,j;
+ float *y;
+ float s;
 
-  /******Implement the solution here******/
-  /***************************************/
+ y = malloc(sizeof(float) * n);
+
+ y[0] = B[0];
+	for (i = 1; i < n; i++)
+	{
+		s=0;
+		for (j = 0; j <= i-1; j++)
+		{
+			s += L[i][j] * y[j];
+		}
+		y[i] = B[i] - s;
+	}
+	return y;
+}
+
+float *remontee(float A[][N], float *y, int n)
+{
+ int i,j;
+ float s;
+ float *x;
 
 
-  /***************************************/
+ x = malloc(sizeof(float) * n);
+
+
+
+ x[n - 1] = y[n - 1] / A[n -1][n -1];
+	for (i = n - 2; i >= 0; i--)
+	{
+		s = 0;
+		for (j = i+1; j <= n - 1; j++)
+			s += A[i][j] * x[j];
+		x[i] = (y[i] - s) / A[i][i];
+	}
+	return x;
+}
+
+ float *lu(float A[][N], float *B, int n)
+{
+  int k,i,j;
+  float L[N][N];
+  float *x;
+  float *y;
+
+
+  for (k = 0; k < n - 1; k++)
+	{
+		for (i = k + 1; i < n; i++)
+		{
+			L[i][k] = A[i][k] / A[k][k];
+			for (j = k + 1; j < n; j++)
+			{
+				A[i][j] = A[i][j] - L[i][k] * A[k][j];
+			}
+		}
+	}
+        /* Calcul The Matrix Y (Methode descente) */
+        
+        y = descente(L, B, n);
+
+	/* Calcul the Matrix X (Methode remontee) */
+
+	x = remontee(A, y, n);
   return (x);
 }
 
